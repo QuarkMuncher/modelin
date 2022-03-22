9e60c6e40c1dae4c840f990a78a2d9519fcc777d
Sebastian Andersen <sebastian@tinx.dk>
2022-03-22 13:03:04 +0100

    Moves Nav to App and adds Jumbotron to Home

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { Route, Routes } from 'react-router-dom'
+import { Nav } from './components'
 import { Home } from './pages'
 
 function App() {
   return (
     <>
+      <Nav />
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
