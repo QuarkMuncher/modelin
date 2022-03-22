2d003cc5db26f1d432a81b1a23ff62297c3f0246
Sebastian Andersen <sebastian@tinx.dk>
2022-03-23 00:20:27 +0100

    Fetches text and passes props to Jumbotron

--- src/components/Jumbotron/Jumbotron.jsx
+++ src/components/Jumbotron/Jumbotron.jsx
@@ -1,10 +1,23 @@
 import { Grid, GridItem, Image } from '@chakra-ui/react'
+import CallToAction from './CallToAction'
 
-export default function Jumbotron({ backgroundSource }) {
+export default function Jumbotron({
+  backgroundSource,
+  title,
+  subtitle,
+  buttonText
+}) {
   return (
-    <Grid templateColumns="repeat(6, 1fr)" templateRows="repeat(3, 1fr)">
-      <GridItem colStart={2} colEnd={-1} rowSpan={3}>
+    <Grid templateColumns="repeat(6, 1fr)" templateRows="16rem 1fr 16rem">
+      <GridItem colStart={2} colEnd={-1} rowStart={1} rowSpan={3}>
         <Image src={backgroundSource} objectFit="cover" />
+      </GridItem>
+      <GridItem colStart={1} colSpan={3} rowStart={2} rowSpan={1}>
+        <CallToAction
+          title={title}
+          subtitle={subtitle}
+          buttonText={buttonText}
+        />
       </GridItem>
     </Grid>
   )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,28 @@
+import { useState, useEffect } from 'react'
 import { Jumbotron } from '../components'
 import jumboBackground from '../assets/images/LandingPageIMG.jpg'
 
 export default function Home() {
+  const [jumbotronData, setJumbotronData] = useState()
+
+  useEffect(() => {
+    fetch(`${import.meta.env.VITE_API_URL}texts/1`)
+      .then(res => res.json())
+      .then(res => {
+        if (res && res[0].description) setJumbotronData(...res)
+      })
+  }, [])
+
   return (
     <>
-      <Jumbotron backgroundSource={jumboBackground} />
+      {jumbotronData && (
+        <Jumbotron
+          backgroundSource={jumboBackground}
+          title={jumbotronData.description}
+          subtitle="Individual approach to the design of private apartments and public areas"
+          buttonText="Portfolio"
+        />
+      )}
     </>
   )
 }
