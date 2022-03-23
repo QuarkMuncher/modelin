a4714379c33b5dd238528a09ca6555accea08755
Sebastian Andersen <sebastian@tinx.dk>
2022-03-23 11:11:02 +0100

    Limits content width to 1920 and makes Nav.jsx root element nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
 import { Route, Routes } from 'react-router-dom'
-import { Box } from '@chakra-ui/react'
+import { Box, Container } from '@chakra-ui/react'
 import { Nav } from './components'
 import { Home } from './pages'
 
 function App() {
   return (
-    <>
+    <Container maxW="container.2xl" p={0}>
       <Nav />
       <Box mt={44}>
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
       </Box>
-    </>
+    </Container>
   )
 }
 
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,4 +1,4 @@
-import { HStack, Image } from '@chakra-ui/react'
+import { HStack, Flex, Spacer, Image } from '@chakra-ui/react'
 import { SiHouzz, SiInstagram } from 'react-icons/si'
 import { Link } from 'react-router-dom'
 import IconLink from './IconLink'
@@ -8,27 +8,34 @@ import Logo from '../../assets/logo/logo-modelin.svg'
 export default function Nav() {
   return (
     <HStack
-      py={16}
-      px={6}
+      as="nav"
+      w="full"
+      justify="center"
       position="fixed"
       top={0}
       left={0}
       right={0}
-      bg="white"
-      w="full"
-      align="flex-end"
-      justify="space-between"
     >
-      <Image src={Logo} alt="Modelin Logo" h="48px" />
-      <HStack align="flex-end" spacing={4}>
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="/portfolio">Portfolio</NavLink>
-        <NavLink to="/prices">Services &amp; Prices</NavLink>
-        <NavLink to="/about">About Me</NavLink>
-        <NavLink to="/contact">Contact</NavLink>
-        <IconLink as={Link} icon={SiInstagram} to="#" />
-        <IconLink as={Link} icon={SiHouzz} to="#" />
-      </HStack>
+      <Flex
+        py={16}
+        px={6}
+        bg="white"
+        align="flex-end"
+        w="full"
+        maxW="container.2xl"
+      >
+        <Image src={Logo} alt="Modelin Logo" h="48px" />
+        <Spacer />
+        <HStack align="flex-end" spacing={4}>
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/portfolio">Portfolio</NavLink>
+          <NavLink to="/prices">Services &amp; Prices</NavLink>
+          <NavLink to="/about">About Me</NavLink>
+          <NavLink to="/contact">Contact</NavLink>
+          <IconLink as={Link} icon={SiInstagram} to="#" />
+          <IconLink as={Link} icon={SiHouzz} to="#" />
+        </HStack>
+      </Flex>
     </HStack>
   )
 }
